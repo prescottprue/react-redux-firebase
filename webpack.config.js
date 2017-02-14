98da458d1ac0be18fe86b920837c6334e80ca88a
prescottprue <sprue.dev@gmail.com>
2017-02-13 22:34:08 -0800

    External config added to Webpack. npm files list updated.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,25 @@ var config = {
     library: 'ReactReduxFirebase',
     libraryTarget: 'umd'
   },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    redux: {
+      commonjs: 'redux',
+      commonjs2: 'redux',
+      amd: 'redux',
+      root: 'Redux'
+    },
+    'react-redux': {
+      commonjs: 'react-redux',
+      amd: 'react-redux',
+      root: 'ReactRedux'
+    }
+  },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
@@ -24,17 +43,12 @@ var config = {
 if (env === 'production') {
   config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
+      comments: false,
       compressor: {
         pure_getters: true,
         unsafe: true,
         unsafe_comps: true,
         warnings: false,
-        screw_ie8: false
-      },
-      mangle: {
-        screw_ie8: false
-      },
-      output: {
         screw_ie8: false
       }
     })
