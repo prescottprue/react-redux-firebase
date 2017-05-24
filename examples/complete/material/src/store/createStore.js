77f2f5ab44d4601bab3b9e2b071ceea21cdccedb
prescottprue <sprue.dev@gmail.com>
2017-05-23 18:36:07 -0700

    Logger added to material example.

--- examples/complete/material/src/store/createStore.js
+++ examples/complete/material/src/store/createStore.js
@@ -1,11 +1,22 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import makeRootReducer from './reducers'
 import { browserHistory } from 'react-router'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reactReduxFirebase, getFirebase, toJS } from 'react-redux-firebase'
+import { createLogger } from 'redux-logger'
 import { firebase as fbConfig, reduxFirebase as reduxConfig } from '../config'
 import { version } from '../../package.json'
+import makeRootReducer from './reducers'
 import { updateLocation } from './location'
+
+// NOTE: Runs an toJS action on every log (DEV ONLY)
+const logger = createLogger({ // eslint-disable-line no-unused-vars
+  stateTransformer: (state) => {
+    if (state.firebase) {
+      return { ...state, firebase: toJS(state.firebase) }
+    }
+    return state
+  }
+})
 
 export default (initialState = {}, history) => {
   // ======================================================
@@ -17,7 +28,8 @@ export default (initialState = {}, history) => {
   // Middleware Configuration
   // ======================================================
   const middleware = [
-    thunk.withExtraArgument(getFirebase)
+    thunk.withExtraArgument(getFirebase),
+    logger, // Uncomment to see actions in console
     // This is where you add other middleware like redux-observable
   ]
 
