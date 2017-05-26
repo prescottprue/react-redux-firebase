4a9761deba37ce4bfa13559b91131935386110f6
prescottprue <sprue.dev@gmail.com>
2017-05-26 16:45:14 -0700

    State query based example updated with loading wrapping and step by step explanation.

---
+++ examples/snippets/stateBasedQuery/Todos.js
@@ -0,0 +1,51 @@
+import react, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import {
+  firebaseConnect,
+  isLoaded,
+  isEmpty,
+  pathToJS,
+  dataToJS
+} from 'react-redux-firebase'
+
+import logo from './logo.svg'
+import TodoItem from './TodoItem'
+
+class Todos extends Component {
+  static propTypes = {
+    auth: PropTypes.shape({
+      uid: PropTypes.string.isRequired
+    }),
+    todos: PropTypes.object
+  }
+
+  render () {
+    const { todos } = this.props
+
+    const todosList = (!isLoaded(todos))
+                        ? 'Loading'
+                        : (isEmpty(todos))
+                          ? 'Todo list is empty'
+                          : Object.keys(todos).map((key) => (
+                            <TodoItem key={key} id={key} todo={todos[key]} />
+                          ))
+    return (
+      <div className='Todos'>
+        {todosList}
+      </div>
+    )
+  }
+}
+
+const fbWrappedComponent = firebaseConnect(({ auth }) => ([ // auth comes from props
+  {
+    path: 'todos',
+    queryParams: ['orderByChild=uid', `equalTo=${auth}`]
+  }
+]))(authWrappedComponent)
+
+export default connect(
+  ({ firebase }) => ({
+    todos: dataToJS(firebase, 'todos')
+  })
+)(fbWrappedComponent)
