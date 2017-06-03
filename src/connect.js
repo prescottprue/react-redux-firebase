cfc3aba95ce6bfae488651f7d69466a54051e2a2
Sergey Martynov <martynovs@gmail.com>
2017-06-03 07:11:56 +0300

    Allow to use different stores in firebaseConnect (#148)
    It helps to use more than one firebase app by creating separate store for each one,
    it may be enough to fix #29

--- src/connect.js
+++ src/connect.js
@@ -32,7 +32,7 @@ import { getEventsFromInput, createCallable } from './utils'
  *   auth: pathToJS(firebase, 'auth') // pass auth data as this.props.auth
  * }))(fbWrapped)
  */
-export default (dataOrFn = []) => WrappedComponent => {
+export const createFirebaseConnect = (storeKey = 'store') => (dataOrFn = []) => WrappedComponent => {
   class FirebaseConnect extends Component {
     constructor (props, context) {
       super(props, context)
@@ -41,11 +41,11 @@ export default (dataOrFn = []) => WrappedComponent => {
     }
 
     static contextTypes = {
-      store: PropTypes.object.isRequired
+      [storeKey]: PropTypes.object.isRequired
     };
 
     componentWillMount () {
-      const { firebase, dispatch } = this.context.store
+      const { firebase, dispatch } = this.context[storeKey]
 
       // Allow function to be passed
       const inputAsFunc = createCallable(dataOrFn)
@@ -94,3 +94,5 @@ export default (dataOrFn = []) => WrappedComponent => {
 
   return hoistStatics(FirebaseConnect, WrappedComponent)
 }
+
+export default createFirebaseConnect()
