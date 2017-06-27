4a7370ee7c2e1123f558f36dd8cad9e50ecba282
Scott Prue <scott@residenetwork.com>
2017-06-27 01:23:35 -0700

    Version v2.0.0-alpha.3
    * Firebase is an external
    * createFirebaseInstance is exposed at top level
    * Removed unnecessary file (old connect file)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import connect from './connect'
+import firebaseConnect, { createFirebaseInstance } from './createFirebaseInstance'
 import compose, { getFirebase } from './compose'
 import reducer from './reducer'
 import constants, { actionTypes } from './constants'
 import * as helpers from './helpers'
 
 export default {
-  firebase: connect,
-  firebaseConnect: connect,
+  firebase: firebaseConnect,
+  firebaseConnect,
+  createFirebaseInstance,
   firebaseStateReducer: reducer,
   reduxReactFirebase: compose,
   reactReduxFirebase: compose,
