96e4eaf8097ee2c508dde0466e4188821e6ce953
prescottprue <sprue.dev@gmail.com>
2017-06-29 17:11:15 -0700

    createFirebaseConnect exposed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createFirebaseInstance } from './createFirebaseInstance'
-import firebaseConnect from './connect'
+import firebaseConnect, { createFirebaseConnect } from './connect'
 import compose, { getFirebase } from './compose'
 import reducer from './reducer'
 import constants, { actionTypes } from './constants'
@@ -8,6 +8,7 @@ import * as helpers from './helpers'
 export default {
   firebase: firebaseConnect,
   firebaseConnect,
+  createFirebaseConnect,
   createFirebaseInstance,
   firebaseStateReducer: reducer,
   reduxReactFirebase: compose,
