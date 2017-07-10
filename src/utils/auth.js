95ac29c32a1d5a2a076971c0e17c4d884ee79d67
Scott Prue <scott@residenetwork.com>
2017-07-10 12:55:27 -0700

    Login now supports credential. Test added to confirm credential functionality.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -57,36 +57,22 @@ export const createAuthProvider = (firebase, providerName, scopes) => {
  * @param {String} credentials.scopes - Scopes to add to provider (i.e. email)
  * @private
  */
-export const getLoginMethodAndParams = (firebase, {email, password, provider, type, token, scopes}) => {
+export const getLoginMethodAndParams = (firebase, {email, password, provider, type, token, scopes, credential}) => {
   if (provider) {
+    if (credential) {
+      return { method: 'signInWithCredential', params: [ credential ] }
+    }
     if (token) {
-      return {
-        method: 'signInWithCredential',
-        params: [ provider, token ]
-      }
+      return { method: 'signInWithCredential', params: [ provider, token ] }
     }
     const authProvider = createAuthProvider(firebase, provider, scopes)
     if (type === 'popup') {
-      return {
-        method: 'signInWithPopup',
-        params: [ authProvider ]
-      }
+      return { method: 'signInWithPopup', params: [ authProvider ] }
     }
-    return {
-      method: 'signInWithRedirect',
-      params: [ authProvider ]
-    }
+    return { method: 'signInWithRedirect', params: [ authProvider ] }
   }
   if (token) {
-    return {
-      method: 'signInWithCustomToken',
-      params: [ token ]
-    }
+    return { method: 'signInWithCustomToken', params: [ token ] }
   }
-  return {
-    method: 'signInWithEmailAndPassword',
-    params: [ email, password ]
-  }
+  return { method: 'signInWithEmailAndPassword', params: [ email, password ] }
 }
-
-export default { getLoginMethodAndParams }
