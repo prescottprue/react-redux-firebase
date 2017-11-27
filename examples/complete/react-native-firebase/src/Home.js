eea7ddfc3fc799f39e16dabd08707ae381591ea2
prescottprue <sprue.dev@gmail.com>
2017-11-27 02:59:38 -0800

    recompose used to simplify react-native-firebase example

--- examples/complete/react-native-firebase/src/Home.js
+++ examples/complete/react-native-firebase/src/Home.js
@@ -2,66 +2,41 @@ import React from 'react'
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { isLoaded, isEmpty, firebaseConnect } from 'react-redux-firebase';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
 import NewTodo from './NewTodo'
-import Todos from './Todos'
+import TodosList from './TodosList'
 
-class Home extends React.Component {
-  state = {
-    text: null
-  }
+const enhnace = compose(
+  connect(({ firebase: { auth } }) => ({
+    uid: auth.uid
+  })),
+  withStateHandlers(
+    ({ initialText = '' }) => ({ newTodoText: initialText }),
+    {
+      onEmailChange: () => email => ({ email }),
+      onPasswordChange: () => password => ({ password }),
+      onNewTodoChange: () => newTodoText => ({ newTodoText })
+    }
+  ),
+  withHandlers({
+    addTodo: props => () =>
+      props.firebase.push('todos', { text: newTodoText || 'Sample Text', owner: props.uid })
+  })
+)
 
-  completeTodo = (key, todo) => {
-    return this.props.firebase.update(`todos/${key}`, { done: !todo.done })
-  }
+const Home = ({ todos, addTodo, onNewTodoChange, newTodoText }) => (
+  <View style={styles.container}>
+    <Text style={styles.header}>Todos</Text>
+    <NewTodo
+      onNewTouch={addTodo}
+      newValue={newTodoText}
+      onInputChange={onNewTodoChange}
+    />
+    <TodosList />
+  </View>
+)
 
-  addTodo = () => {
-    const { text } = this.state;
-    return this.props.firebase.push('todos', { text, completed: false });
-  }
-
-  render() {
-    const { todos } = this.props;
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Todos</Text>
-        <NewTodo
-          onNewTouch={this.addTodo}
-          newValue={this.state.text}
-          onInputChange={(v) => this.setState({text: v})}
-        />
-        {
-          !isLoaded(todos)
-            ? <ActivityIndicator size="large" style={{ marginTop: 100 }}/>
-            : null
-        }
-        {
-          isLoaded(todos) && !isEmpty(todos)
-            ?
-              <Todos
-                todos={todos}
-                onItemTouch={this.completeTodo}
-              />
-            :
-              <View style={styles.container}>
-                <Text>No Todos Found</Text>
-              </View>
-        }
-      </View>
-    );
-  }
-}
-
-export default compose(
-  firebaseConnect([
-    { path: 'todos', queryParams: ['limitToLast=10'] } // create listener for firebase data -> redux
-  ]),
-  connect((state) => ({
-    // todos: state.firebase.data.todos, // todos data object from redux -> props.todos
-    todos: state.firebase.ordered.todos, // todos ordered array from redux -> props.todos
-  }))
-)(Home);
+export default enhance(Home);
 
 const styles = StyleSheet.create({
   container: {
