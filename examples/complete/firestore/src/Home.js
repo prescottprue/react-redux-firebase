7b4873aba8d3370f0bde4601eeab365afffed960
prescottprue <sprue.dev@gmail.com>
2017-11-27 00:42:55 -0800

    Update Firestore example - #344

--- examples/complete/firestore/src/Home.js
+++ examples/complete/firestore/src/Home.js
@@ -2,63 +2,68 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
+import { withHandlers } from 'recompose'
 import {
   firestoreConnect,
   isLoaded,
   isEmpty
 } from 'react-redux-firebase'
 import TodoItem from './TodoItem'
+import NewTodo from './NewTodo'
 import './App.css'
 import { firebase as firebaseConf } from './config'
 
-const Home = ({ firestore, todos }) => {
-  const handleAdd = () => firestore.add('todos', { text: 'sample', done: false })
-  console.log('todos;', todos)
-  return (
-    <div className='App'>
-      <div className='App-header'>
-        <h2>firestore demo</h2>
-      </div>
-      <div className='App-todos'>
-        <h4>
-          Loaded From
-          <span className='App-Url'>
-            <a href={firebaseConf.databaseURL}>
-              {firebaseConf.databaseURL}
-            </a>
-          </span>
-        </h4>
-        <h4>Todos List</h4>
-        {
-          !isLoaded(todos)
-            ? 'Loading'
-            : isEmpty(todos)
-              ? 'Todo list is empty'
-              : todos.map((todo) =>
-                  <TodoItem key={todo.id} todo={todo} />
-                )
-        }
-        <h4>New Todo</h4>
-        <button onClick={handleAdd}>
-          Add
-        </button>
-      </div>
-    </div>
-  )
-}
-
-Home.propTypes = {
-  firestore: PropTypes.shape({
-    add: PropTypes.func.isRequired,
-  }),
-  todos: PropTypes.array
-}
-
-export default compose(
-  firestoreConnect([{ collection: 'todos', where: ['done', '==', false] }]),
+const enhance = compose(
+  firestoreConnect([
+    // Load todos from Firestore which are not done into redux
+    { collection: 'todos', where: ['done', '==', false] }
+  ]),
   connect(
     ({ firestore }) => ({
       todos: firestore.ordered.todos,
     })
-  )
-)(Home)
+  ),
+  withHandlers({
+    addTodo: props => () =>
+      props.firestore.add('todos', { text: 'sample', done: false })
+  })
+)
+
+const Home = ({ firestore, todos, addTodo }) => (
+  <div className='App'>
+    <div className='App-header'>
+      <h2>firestore demo</h2>
+    </div>
+    <div className='App-todos'>
+      <h4>
+        Loaded From
+        <span className='App-Url'>
+          <a href={firebaseConf.databaseURL}>
+            {firebaseConf.databaseURL}
+          </a>
+        </span>
+      </h4>
+      <h4>Todos List</h4>
+      {
+        !isLoaded(todos)
+          ? 'Loading'
+          : isEmpty(todos)
+            ? 'Todo list is empty'
+            : todos.map((todo) =>
+                <TodoItem key={todo.id} todo={todo} />
+              )
+      }
+      <NewTodo />
+    </div>
+  </div>
+)
+
+Home.propTypes = {
+  firestore: PropTypes.shape({ // from enhnace (withFirestore)
+    add: PropTypes.func.isRequired,
+  }),
+  addTodo: PropTypes.func.isRequired, // from enhance (withHandlers)
+  todos: PropTypes.array
+}
+
+export default enhance(Home)
