cac216dda59ef68c5ee65f981717bff4f276f54e
Scott Prue <scott@residebrokerage.com>
2018-05-29 18:14:42 -0700

    v2.1.5
    * fix(storage): uploadFile no longer errors out due to downloadURLs not being defined (v5 Firebase SDK) - #480
    * fix(examples): add recompose to firestore example's dependencies

--- examples/complete/firestore/src/config.js
+++ examples/complete/firestore/src/config.js
@@ -1,10 +1,10 @@
 export const firebase = {
-  apiKey: "AIzaSyBTvAcJwsN8iygsnwAZyzIuy1uleYEpWIo",
-  authDomain: "redux-firestore.firebaseapp.com",
-  databaseURL: "https://redux-firestore.firebaseio.com",
-  projectId: "redux-firestore",
-  storageBucket: "redux-firestore.appspot.com",
-  messagingSenderId: "502471151289"
+  apiKey: "AIzaSyCTUERDM-Pchn_UDTsfhVPiwM4TtNIxots",
+  authDomain: "redux-firebasev3.firebaseapp.com",
+  databaseURL: "https://redux-firebasev3.firebaseio.com",
+  projectId: "redux-firebasev3",
+  storageBucket: "redux-firebasev3.appspot.com",
+  messagingSenderId: "823357791673"
 }
 
 export const rrfConfig = {
