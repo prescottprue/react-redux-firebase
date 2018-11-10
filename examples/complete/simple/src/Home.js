16127e1de15d0f9ebc0e3ef7bbf8369034fedea0
prescottprue <sprue.dev@gmail.com>
2018-11-09 18:36:22 -0800

    fix(examples): update simple example to use newest create-react-app results (including react 16.6)

--- examples/complete/simple/src/Home.js
+++ examples/complete/simple/src/Home.js
@@ -10,55 +10,58 @@ import {
 import logo from './logo.svg'
 import TodoItem from './TodoItem'
 import './App.css'
-import GoogleButton from 'react-google-button'
+import NewTodo from './NewTodo';
 
-const Home = ({ firebase, todos }) => {
-  const handleAdd = () => {
-    return firebase.push('/todos', { text: this.input.value, done: false })
-      .then(() => {
-        this.input.value = ''
-      })
-  }
-  return (
-    <div className='App'>
-      <div className='App-header'>
-        <h2>react-redux-firebase demo</h2>
-        <img src={logo} className='App-logo' alt='logo' />
-      </div>
-      <div className='App-todos'>
-        <h4>
-          Loaded From
-          <span className='App-Url'>
-            <a href='https://redux-firebasev3.firebaseio.com/'>
-              redux-firebasev3.firebaseio.com
-            </a>
-          </span>
-        </h4>
-        <h4>Todos List</h4>
-        {
-          !isLoaded(todos)
-            ? 'Loading'
-            : isEmpty(todos)
-              ? 'Todo list is empty'
-              : Object.keys(todos).map((key) => (
-                <TodoItem key={key} id={key} todo={todos[key]} />
-              ))
-        }
-        <h4>New Todo</h4>
-        <input type='text' ref={ref => { this.input = ref }} />
-        <button onClick={handleAdd}>
-          Add
-        </button>
-      </div>
+const Home = ({ firebase, todos }) => (
+  <div className='App'>
+    <div className='App-header'>
+      <h2>react-redux-firebase demo</h2>
+      <img src={logo} className='App-logo' alt='logo' />
     </div>
-  )
+    <div className='App-todos'>
+      <h4>
+        Loaded From
+        <span className='App-Url'>
+          <a href='https://redux-firebasev3.firebaseio.com/'>
+            redux-firebasev3.firebaseio.com
+          </a>
+        </span>
+      </h4>
+      <h4>Todos List</h4>
+      {
+        !isLoaded(todos)
+          ? 'Loading'
+          : isEmpty(todos)
+            ? 'Todo list is empty'
+            : todos.reverse().map((todo, ind) => (
+              <TodoItem
+                key={`${todo.key}-${ind}`}
+                id={todo.key}
+                todo={todo.value}
+              />
+            ))
+      }
+      <NewTodo />
+    </div>
+  </div>
+)
+
+Home.propTypes = {
+  firebase: PropTypes.object.isRequired
 }
 
-export default compose(
-  firebaseConnect(['todos']),
+const enhance = compose(
+  firebaseConnect(() => [
+    {
+      path: 'todos',
+      queryParams: ['limitToLast=10']
+    }
+  ]),
   connect(
     ({ firebase }) => ({
-      todos: firebase.data.todos,
+      todos: firebase.ordered.todos,
     })
   )
-)(Home)
+)
+
+export default enhance(Home)
--- examples/complete/simple/src/TodoItem.js
+++ examples/complete/simple/src/TodoItem.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { firebase } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { withHandlers, branch, renderNothing } from 'recompose'
+import { withFirebase } from 'react-redux-firebase'
 
 import './Todo.css'
 
-class TodoItem extends Component {
-  static propTypes = {
-    todo: PropTypes.object,
+// Create enhancer to wrap component below. It does the following
+// 1. Renders nothing if todo is undefined
+// 2. Adds props.firebase (used in handlers)
+// 3. Adds toggleDone and deleteTodo handlers (which use props.firebase)
+const enhance = compose(
+  // Render nothing if todo is not defined
+  branch(({ todo }) => !todo, renderNothing),
+  // Add props.firebase
+  withFirebase,
+  // Handlers as props
+  withHandlers({
+    toggleDone: ({ firebase, todo, id }) => () =>
+      firebase.update(`todos/${id}`, { done: !todo.done }),
+    deleteTodo: ({ firebase, todo, id }) => () =>
+      firebase.remove(`todos/${id}`)
+  })
+)
+
+const TodoItem = ({ deleteTodo, toggleDone, todo }) => (
+  <li className="Todo">
+    <input
+      className="Todo-Input"
+      type="checkbox"
+      checked={todo.done}
+      onChange={toggleDone}
+    />
+    {todo.text || todo.name}
+    <button className="Todo-Button" onClick={deleteTodo}>
+      Delete
+    </button>
+  </li>
+)
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    text: PropTypes.string,
     id: PropTypes.string
-  }
-
-  render(){
-    const {firebase, todo, id} = this.props
-    const toggleDone = () => {
-      firebase.set(`/todos/${id}/done`, !todo.done)
-    }
-
-    const deleteTodo = (event) => {
-       firebase.remove(`/todos/${id}`)
-    }
-    return (
-      <li className="Todo">
-        <input
-          className="Todo-Input"
-          type="checkbox"
-          checked={todo.done}
-          onChange={toggleDone}
-        />
-        {todo.text || todo.name}
-        <button className="Todo-Button" onClick={deleteTodo}>
-          Delete
-        </button>
-      </li>
-    )
-  }
+  }),
+  firebase: PropTypes.shape({ // from enhnace (withfirebase)
+    update: PropTypes.func.isRequired,
+    remove: PropTypes.func.isRequired
+  }),
+  toggleDone: PropTypes.func, // from enhance (withHandlers)
+  deleteTodo: PropTypes.func, // from enhance (withHandlers)
 }
-export default firebase()(TodoItem)
+
+export default enhance(TodoItem)
