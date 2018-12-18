f168caaa7cda125dd351dc3b421dc9881f044667
Scott Prue <scott@residebrokerage.com>
2018-12-17 20:42:47 -0800

    Add lodash-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,70 @@
 
 const webpack = require('webpack')
 const pkg = require('./package.json')
-const env = process.env.NODE_ENV
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
-const config = {
-  module: {
-    rules: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
-    ]
-  },
-  output: {
-    library: 'ReactReduxFirebase',
-    libraryTarget: 'umd'
-  },
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React'
-    },
-    firebase: {
-      commonjs: 'firebase',
-      commonjs2: 'firebase',
-      amd: 'firebase',
-      root: 'Firebase'
-    },
-    'prop-types': {
-      commonjs: 'prop-types',
-      commonjs2: 'prop-types',
-      amd: 'prop-types',
-      root: 'PropTypes'
-    }
-  },
-  plugins: []
-}
+module.exports = (env, argv) => {
+  const { mode } = argv
 
-if (config.mode === 'production') {
-  if (process.env.SIZE) {
-    config.plugins.push(new BundleAnalyzerPlugin())
-  }
-}
-
-config.plugins.push(
-  new webpack.BannerPlugin(
-    {
-      banner: `${pkg.name}${env === 'production' ? '.min' : ''}.js v${pkg.version}`,
+  const plugins = [
+    new LodashModuleReplacementPlugin(),
+    new webpack.BannerPlugin({
+      banner: `${pkg.name}${mode === 'production' ? '.min' : ''}.js v${
+        pkg.version
+      }`,
       raw: false,
       entryOnly: true
-    }
-  )
-)
+    })
+  ]
 
-module.exports = config
+  if (mode === 'production') {
+    if (env && env.size) {
+      plugins.push(new BundleAnalyzerPlugin())
+    }
+  }
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loaders: ['babel-loader'],
+          exclude: /node_modules/
+        }
+      ]
+    },
+    output: {
+      library: 'ReactReduxFirebase',
+      libraryTarget: 'umd'
+    },
+    externals: {
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React'
+      },
+      'react-is': {
+        commonjs: 'react-is',
+        commonjs2: 'react-is',
+        amd: 'react-is',
+        root: 'ReactIs'
+      },
+      firebase: {
+        commonjs: 'firebase',
+        commonjs2: 'firebase',
+        amd: 'firebase',
+        root: 'Firebase'
+      },
+      'prop-types': {
+        commonjs: 'prop-types',
+        commonjs2: 'prop-types',
+        amd: 'prop-types',
+        root: 'PropTypes'
+      }
+    },
+    plugins
+  }
+}
