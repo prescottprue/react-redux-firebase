f50e094f5fb7f340d270cf7c9f22e28ca5092aae
Scott Prue <scott@residebrokerage.com>
2018-12-17 22:16:48 -0800

    fix(docs): simplify and add comment to stateBasedQuery example snippet

--- examples/snippets/stateBasedQuery/Home.js
+++ examples/snippets/stateBasedQuery/Home.js
@@ -2,24 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
+import Todos from './Todos'
+import LoginView from './LoginView'
 
-const Home = ({ auth }) => {
+function Home({ auth }) {
   // handle initial loading of auth
   if (!isLoaded(auth)) {
     return <div>Loading...</div>
   }
 
+  // User is not logged in, show login view
   if (isEmpty(auth)) {
     return <LoginView />
   }
 
-  return <TodosView auth={auth} />
+  return <Todos uid={auth.uid} />
 }
 
 Home.propTypes = {
   auth: PropTypes.object
 }
 
-export default connect(
-  ({ firebase: { auth } }) => ({ auth })
-)(Home)
+export default connect(state => ({
+  auth: state.firebase.auth
+}))(Home)
