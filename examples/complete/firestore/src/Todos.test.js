cdac5808b1e0ab0a9578576ef04439077446232d
Harish Rana <r.shivi@yahoo.com>
2020-01-07 13:47:46 +0530

    style(example): change the global variables to local variables

--- examples/complete/firestore/src/Todos.test.js
+++ examples/complete/firestore/src/Todos.test.js
@@ -6,9 +6,8 @@ import Todos from './Todos'
 
 jest.mock('react-redux')
 
-describe('Should test the sample component', () => {
+describe('Should test the Todos Component', () => {
   let container = null
-  const expectedTodos = []
 
   beforeEach(() => {
     // setup a DOM element as a render target
@@ -23,28 +22,35 @@ describe('Should test the sample component', () => {
     container = null
   })
 
-  it('show empty text when there is no text', () => {
-    useSelector.mockReturnValue(expectedTodos)
+  it('show empty text when the todo list is empty', () => {
+    let todos = []
+    useSelector.mockReturnValue(todos)
+
     act(() => {
       render(<Todos />, container)
     })
-    const listItem = container.querySelectorAll('li')
+
+    container.querySelectorAll('li')
     expect(container.textContent).toBe('Todo list is empty')
   })
 
-  it('should have on TodoItem when there is one to do', () => {
-    let moreTodo = {
-      id: 'todoId',
-      name: 'Sample todo',
-      text: 'This is for testing todos'
-    }
-    expectedTodos.push(moreTodo)
-    useSelector.mockReturnValue(expectedTodos)
+  it('should have one TodoItem in the list', () => {
+    let todos = [
+      {
+        id: 'todoId',
+        name: 'Sample todo',
+        text: 'This is for testing todos'
+      }
+    ]
+
+    useSelector.mockReturnValue(todos)
     act(() => {
       render(<Todos />, container)
     })
+
     const listItems = container.querySelectorAll('li')
-        // this is not a real id, so only text which can be visible is Delete
+
+    // this is not a real id, so only text which can be visible is Delete
     expect(listItems[0].textContent).toBe('Delete')
   })
 })
